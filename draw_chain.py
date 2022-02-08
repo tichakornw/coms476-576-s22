f12ca419b2343d57e315b47442458ad4153b0e76
Nok Wongpiromsarn <tichakorn@gmail.com>
2022-02-08 11:23:51 -0600

    Add draw_chain for drawing a kinematic chain in homework 1

---
+++ draw_chain.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+
+
+def plot_chain(joint_positions, link_vertices):
+    """Plot a 2D kinematic chain A_1, ..., A_m
+
+    @type joint_positions: a list [p_1, ..., p_{m+1}] where p_i is the position [x,y] of the joint between A_i and A_{i-1}
+    @type link_vertices: a list [V_1, ..., V_m] where V_i is the list of [x,y] positions of vertices of A_i
+    """
+
+    fig, ax = plt.subplots()
+    plot_links(link_vertices, ax)
+    plot_joints(joint_positions, ax)
+    ax.axis("equal")
+    plt.show()
+
+
+def plot_links(link_vertices, ax):
+    """Plot the links of a 2D kinematic chain A_1, ..., A_m on the axis ax
+
+    @type link_vertices: a list [V_1, ..., V_m] where V_i is the list of [x,y] positions of vertices of A_i
+    """
+
+    for vertices in link_vertices:
+        x = [vertex[0] for vertex in vertices]
+        y = [vertex[1] for vertex in vertices]
+
+        x.append(vertices[0][0])
+        y.append(vertices[0][1])
+        ax.plot(x, y, "k-", linewidth=2)
+
+
+def plot_joints(joint_positions, ax):
+    """Plot the joints of a 2D kinematic chain A_1, ..., A_m on the axis ax
+
+    @type joint_positions: a list [p_1, ..., p_{m+1}] where p_i is the position [x,y] of the joint between A_i and A_{i-1}
+    """
+    x = [pos[0] for pos in joint_positions]
+    y = [pos[1] for pos in joint_positions]
+    ax.plot(x, y, "k.", markersize=10)
